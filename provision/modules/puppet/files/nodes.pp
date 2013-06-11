af1d77c429ed8ddcfb1b79724bb2dcac1e36c584
lionel tabard <tabard.lionel@gmail.com>
2013-06-11 09:44:56 +0200

    ajout du module vsftpd-thias

--- provision/modules/puppet/files/nodes.pp
+++ provision/modules/puppet/files/nodes.pp
@@ -87,8 +87,55 @@ node 'vento' {
 sysctl { "net.ipv4.ip_forward":
     ensure  => present,
     value   => "1",
-    comment => "net.ipv4.ip_forward 1",
+    comment => "net.ipv4.ip_forward 1", 
   }
+ resources { "firewall":
+    purge => true
+  }
+
+  firewall {
+    '000 accept all icmp ' :
+      proto => 'icmp',
+      action => 'accept';
+
+    '001 accept inbound ssh ' :
+      proto => 'tcp',
+      port => 22,
+      action => 'accept';
+
+    '002 accept local traffic' :
+    # traffic within localhost is OK
+      iniface => 'lo',
+      action => 'accept';
+
+    '003 allow established connections':
+      proto   => 'all',
+      # this is needed to make outbound connections work, such as database connection
+      state => ['RELATED','ESTABLISHED'],
+      action => 'accept';
+
+    '100 accept inbound http' :
+      proto => 'tcp',
+      port => 80,
+      action => 'accept';
+
+    '101 accept inbound https ' :
+      proto => 'tcp',
+      port => 443,
+      action => 'accept';
+
+    '200 accept inbound ftp ' :
+      proto => 'tcp',
+      port => 21,
+      action => 'accept';
+
+ }     
+  class { 'vsftpd':
+  anonymous_enable  => 'NO',
+  write_enable      => 'YES',
+  ftpd_banner       => 'Marmotte FTP Server',
+  chroot_local_user => 'YES',
+}
 }
 ##################################
 node 'centos64vb' {
