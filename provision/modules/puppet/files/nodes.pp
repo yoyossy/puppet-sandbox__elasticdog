4ece1f860e8dcd1844316cb3deefb480b4faa1a4
lionel tabard <tabard.lionel@gmail.com>
2013-06-11 15:39:28 +0200

    amelioration   de vsftpd

--- provision/modules/puppet/files/nodes.pp
+++ provision/modules/puppet/files/nodes.pp
@@ -137,6 +137,81 @@ sysctl { "net.ipv4.ip_forward":
   chroot_local_user => 'YES',
 }
 }
+#############################
+node 'dv7' {
+  class { 'helloworld': }
+  class { 'networking': }
+
+  network::if::static { 'eth0':
+  ensure       => 'up',
+  ipaddress    => '10.53.213.111',
+  netmask      => '255.255.255.0',
+  gateway      => '10.53.213.254',
+#  macaddress   => '08:00:27:FC:7E:E0',
+#  mtu          => '1500',
+#  ethtool_opts => 'speed 1000 duplex full autoneg off',
+}
+
+sysctl { "net.ipv4.ip_forward":
+    ensure  => present,
+    value   => "1",
+    comment => "net.ipv4.ip_forward 1", 
+  }
+ resources { "firewall":
+    purge => true
+  }
+
+  firewall {
+    '000 accept all icmp ' :
+      proto => 'icmp',
+      action => 'accept';
+
+    '001 accept inbound ssh ' :
+      proto => 'tcp',
+      port => 22,
+      action => 'accept';
+
+    '002 accept local traffic' :
+    # traffic within localhost is OK
+      iniface => 'lo',
+      action => 'accept';
+
+    '003 allow established connections':
+      proto   => 'all',
+      # this is needed to make outbound connections work, such as database connection
+      state => ['RELATED','ESTABLISHED'],
+      action => 'accept';
+
+    '100 accept inbound http' :
+      proto => 'tcp',
+      port => 80,
+      action => 'accept';
+
+    '101 accept inbound https ' :
+      proto => 'tcp',
+      port => 443,
+      action => 'accept';
+
+    '200 accept inbound ftp ' :
+      proto => 'tcp',
+      port => 21,
+      action => 'accept';
+
+ }     
+  class { 'vsftpd':
+  anonymous_enable  => 'NO',
+  local_enable            => 'YES',
+  write_enable      => 'YES',
+  ftpd_banner       => 'Marmotte FTP Server',
+  chroot_local_user => 'NO',
+  chroot_list_enable      => 'NO',
+  anon_upload_enable => 'YES',
+  anon_mkdir_write_enable => 'YES',
+  local_umask => '077',
+}
+}
+
+
 ##################################
 node 'centos64vb' {
   class { 'helloworld': }
